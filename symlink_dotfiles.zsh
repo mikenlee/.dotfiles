31a7ec513e7c416c4168666729a55d4abf2c4d68
Mike Lee <none>
2024-03-08 20:39:47 -0700

    moved around files

---
+++ symlink_dotfiles.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Define the path to the dot files directory
+DOTFILES_DIR="/Users/admin/dotfiles"
+
+# Read the configuration file listing dot files and their paths
+while IFS= read -r line; do
+    # Split each line into dot file and target path
+    dotfile=$(echo $line | cut -d ' ' -f 1)
+    target_path=$(echo $line | cut -d ' ' -f 2)
+
+    # Create symbolic link from dot files directory to home directory
+    ln -s "$DOTFILES_DIR/$dotfile" "$HOME/$target_path"
+done < "install.conf"
+
+echo "Symbolic links created successfully."
